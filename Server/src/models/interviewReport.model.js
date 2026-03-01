81d6231bff9873f0b8051cb1c164eced69f44e61
Siddhant Yadav <sidhantyadav1999@gmail.com>
2026-03-01 22:22:59 +0530

    feat: add Mongoose model for interview reports.

---
+++ Server/src/models/interviewReport.model.js
@@ -0,0 +1,86 @@
+import mongoose from "mongoose";
+
+const technicalQuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, "Question is required"]
+    },
+    intention: {
+        type: String,
+        required: [true, "Intention is required"]
+    },
+    answer: {
+        type: String,
+        required: [true, "Answer is required"]
+    }
+}, {
+    _id: false
+})
+
+const behavioralQuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, "Question is required"]
+    },
+    intention: {
+        type: String,
+        required: [true, "Intention is required"]
+    },
+    answer: {
+        type: String,
+        required: [true, "Answer is required"]
+    }
+}, {
+    _id: false
+})
+
+const skillsGapSchema = new mongoose.Schema({
+    skill: {
+        type: String,
+        required: [true, "Skill is required"]
+    },
+    severity: {
+        type: String,
+        enum: ["Low", "Medium", "High"],
+        required: [true, "Severity is required"]
+    }
+}, {
+    _id: false
+})
+
+const preprationplansSchema = new mongoose.Schema({
+    day: {
+        type: String,
+        required: [true, "Day is required"]
+    },
+    focus: {
+        type: String,
+        required: [true, "Focus is required"]
+    },
+    tasks: [{
+        type: String,
+        required: [true, "Task is required"]
+    }]
+})
+
+const interviewReportSchema = new mongoose.Schema({
+    jobDescription: {
+        type: String,
+        required: [true, "Job description is required"]
+    },
+    resume: {
+        type: String,
+    },
+    selfDescription: {
+        type: String,
+    },
+    matchScore: {
+        type: Number,
+        min: 0,
+        max: 100
+    },
+    technicalQuestions: [technicalQuestionSchema],
+    behavioralQuestions: [behavioralQuestionSchema],
+    skillsGap: [skillsGapSchema],
+    preprationplans: [preprationplansSchema]
+});
